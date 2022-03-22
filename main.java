e206dd806b76654baf0f4db01e7629da3ce9995e
songyunhwa <happy0155@naver.com>
2022-03-23 01:30:53 +0900

    0323

--- main.java
+++ main.java
@@ -3,66 +3,79 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-	// 줄세우기 - 위상정렬 이중리스트 만들어서 + 해주기?
-	static class line implements Comparable<line>{
+	public static class line {
 		int i;
-		int num;
-		public line(int i, int num){
-			this.i = i;
-			this.num = num;
+		int j;
+		int x_cnt;
+		public line(int i, int j, int x_cnt){
+			this.i=i;
+			this.j=j;
+			this.x_cnt = x_cnt;
 		}
-		@Override
-		public int compareTo(line h2){
-			if(this.num > h2.num)return 1;
-			return -1;
+		
+	}
+	public static int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+	public static int result = Integer.MAX_VALUE;
+	public static void bfs(String[][] arr , int i, int j, int i1, int j1){
+		Queue<line> q = new LinkedList<>();
+		q.add(new line(i, j,  0));
+		int [][] visit= new int[arr.length][arr[0].length];
+		while(!q.isEmpty()){
+			line f= q.poll();
+			visit[f.i][f.j]=  1;
+			
+
+			for(int t=0; t<4; t++){
+				int x = f.i + dir[t][0];
+				int y = f.j + dir[t][1];
+		
+				if(x>=0 && x<arr.length && y>=0 && y<arr[0].length){
+					if( arr[x][y].equals("L")  && visit[x][y] ==0){
+						result = Math.min(result, f.x_cnt/2 + f.x_cnt%2);
+						continue;
+					}	
+
+					if(visit[x][y] == 0){
+					if(arr[x][y].equals("X"))
+					q.add(new line(x,y, f.x_cnt+1));
+					else
+					q.add(new line(x,y, f.x_cnt));
+					}
+
+				}	
+
+			}
 		}
 	}
    	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       String[] strs = br.readLine().split(" ");
-		int n = Integer.parseInt(strs[0]);
-		int m = Integer.parseInt(strs[1]);
+		String[] str = br.readLine().split(" ");
+		int n = Integer.parseInt(str[0]);
+		int k =Integer.parseInt(str[1]);
 
-		ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-		for(int i=0; i<=n; i++){
-			arr.add(new ArrayList<Integer>());
-		}
-		int[] de = new int[n+1];
-		for(int i=0; i<m; i++){
-			strs = br.readLine().split(" ");
-			int a= Integer.parseInt(strs[0]);
-			int b= Integer.parseInt(strs[1]);
-			arr.get(a).add(b);
-			de[b]++;
-		}
-		Queue<Integer> q = new LinkedList<>();
-		ArrayList<Integer> arrs = new ArrayList<Integer>();
+		int x=-1;
+		int y=-1;
+		int x1=0;
+		int y1=0;
+		String[][] arr = new String[n][k];
+		for(int i=0; i<n; i++){
+			String str1 =br.readLine();
+			for(int j=0; j<k; j++){
+				arr[i][j] = str1.substring(j, j+1);
 
-		// 진입차수 0 인것부터
-		for(int i=1; i<=n; i++){
-			if(de[i] == 0) {
-				q.add(i);
-			}
-		}
-		while(!q.isEmpty()){
-			int i = q.poll();
-			arrs.add(i);
-			for(int j=0; j<arr.get(i).size(); j++){
-				de[arr.get(i).get(j)]-=1;
-				if(de[arr.get(i).get(j)] == 0){
-					q.add(arr.get(i).get(j));
+				if(arr[i][j].equals("L")){
+					if(x==-1 && y==-1){
+						x=i;
+						y=j;
+					}else {
+						x1=i;
+						y1=j;
+					}
 				}
 			}
-			
 		}
-	
-		for(int i=1; i<=n; i++){
-			if(!arrs.contains(i))arrs.add(i);
-		}
-		for(int i=0; i< arrs.size(); i++)	
-		System.out.print(arrs.get(i) + " ");
 
-	
+		bfs(arr, x, y ,x1, y1);
+		System.out.println(result);
 	}
 }
