b9bfb8d372617ec261d73f7839a014e28b8726da
songyunhwa <happy0155@naver.com>
2022-06-01 16:31:26 +0900

    0601

--- main.java
+++ main.java
@@ -2,83 +2,28 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	public static int[][] arr;
-	public static int[] paper;
-	public static int ans = Integer.MAX_VALUE;
-	public static boolean check(int i, int j, int k){
 
-		for(int x=i; x<i+k; x++){
-			for(int y=j; y<j+k; y++){
-				if(x > 9 || y> 9) return false;
-				if(arr[x][y] == 0) return false;
-			}
-		}
-		return true;
-	}
-	public static void attach(int i, int j, int k, int d){
-		for(int x=i; x<i+k; x++){
-			for(int y=j; y<j+k; y++){
-				arr[x][y] = d;
-			}
-		}
-	
-	}
-	public static void dfs(int i , int j, int cnt){
-			if(j >= 9 && i>= 9) {
-				ans = Math.min(ans , cnt);
-				return;
-			}
-			if(j > 9) {
-				dfs(i+1 , 0, cnt);
-				return;
-			}
-
-			if(ans <= cnt) return;
-
-			if(arr[i][j] == 1){
-					for(int k=1; k<=5; k++)
-						if(check(i , j ,k)){
-							if(paper[k] > 0){
-							paper[k]-=1;
-							attach(i , j , k, 0);
-							
-							dfs(i, j+1 , cnt+1);
-							
-							paper[k]+=1;
-							attach(i , j , k, 1);
-							}
-						}
-			}
-			else
-			dfs(i, j+1 , cnt);
-		
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		String[] str;
-		arr = new int[10][10];
-		int s=0;
-		paper = new int[6];
-		Arrays.fill(paper, 5);
-		for(int i=0; i<10 ; i++){
+		for(int i=0; i<n; i++) {
 			str = br.readLine().split(" ");
-			for(int j=0; j<10; j++){
-				arr[i][j] =Integer.parseInt(str[j]);
-				if(arr[i][j] == 1){
-					s++;
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[1]);
+
+			str = br.readLine().split(" ");
+			int[] arr =new int[a+1];
+			for(int j=0; j<a; j++){
+				arr[j+1] = Integer.parseInt(str[j]);
+			}
+			ArrayList<ArrayList<>> arr1 = new ArrayList<>();
+			for(int j=0; j<b; j++){
+				str = br.readLine().split(" ");
+				for(int k=0; k<2; k++){
+					
 				}
 			}
 		}
-		if(s==0){
-			System.out.println(0);
-			return;
-		}
-
-		dfs(0,0,0);
-		if(ans == Integer.MAX_VALUE)
-		System.out.println(-1);
-		else System.out.println(ans);
-
-
 	}
 }
