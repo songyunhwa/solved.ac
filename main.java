a2ecb13195b76d8dd9121eac9ef7a05c75ff4b0c
songyunhwa <happy0155@naver.com>
2022-06-02 20:48:10 +0900

    0602

--- main.java
+++ main.java
@@ -2,28 +2,33 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		String[] str;
-		for(int i=0; i<n; i++) {
-			str = br.readLine().split(" ");
-			int a = Integer.parseInt(str[0]);
-			int b = Integer.parseInt(str[1]);
+		String str = br.readLine();
+		String str1 = br.readLine();
 
-			str = br.readLine().split(" ");
-			int[] arr =new int[a+1];
-			for(int j=0; j<a; j++){
-				arr[j+1] = Integer.parseInt(str[j]);
-			}
-			ArrayList<ArrayList<>> arr1 = new ArrayList<>();
-			for(int j=0; j<b; j++){
-				str = br.readLine().split(" ");
-				for(int k=0; k<2; k++){
-					
+		Stack st = new Stack<>();
+		for(int i=0;i <str.length(); i++){
+			st.add(str.substring(i, i+1));
+			if(st.size()>= str1.length()) {
+				boolean eq = true;
+				for(int j=0; j<str1.length(); j++){
+					if(!st.get(st.size() - str1.length() + j).equals(str1.substring(j, j+1))){
+						eq = false;
+						break;
+					}
+				}
+				if(eq){
+					for(int j=0; j<str1.length(); j++) st.pop();
 				}
 			}
 		}
+
+		StringBuilder sb = new StringBuilder();
+		while(!st.isEmpty()){
+		  sb.insert(0, st.pop());
+		}
+
+        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
 	}
 }
